7408901c213e8a5b91e1d044d9a7a46d64da0d5e
Vit_sh <vithits@yandex.ru>
2020-10-15 11:50:43 +0300

    initial commit, gitignore

---
+++ Pryaniki_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pryaniki_test
+//
+//  Created by Vitaly_Shishlyannikov on 15.10.2020.
+//  Copyright © 2020 Vitaly_Shishlayannikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
